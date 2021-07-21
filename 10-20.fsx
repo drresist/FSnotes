be3d24cc7055ce8a5aac099bd8886b74d40f1f4b
DrResist <DrResist@gmail.com>
2021-07-21 22:26:50 +0300

    add generics

--- 10-20.fsx
+++ 10-20.fsx
@@ -25,3 +25,32 @@ module Errors =
                 printfn "Error is %s" e.Message
 
 // 11. Generics
+
+module GenericDU = 
+    type Optional<'T> = 
+        | Something of 'T
+        | Nothing
+    module Optional = 
+        let defaultValue (d : 'T) (optional : Optional<'T>) = 
+            match optional with
+            | Something v -> v
+            | Nothing -> d
+module GenericPoints = 
+    type Point<'T> =  // 'T - generic 
+        {
+            X: 'T
+            Y: 'T
+        }
+    module Point = 
+        let inline moveBy (dx:'T) (dy:'T) (p:Point<'T>) = 
+            {
+                X = p.X + dx
+                Y = p.Y + dy
+            }
+    let pFloat = {X = 1.0; Y = 2.0}
+    let pFloat2 = pFloat |> Point.moveBy 3.0 4.0
+    printfn "pFloat: %A pFloat2: %A" pFloat pFloat2
+
+    let pFloatInt = {X = 1; Y = 2}
+    let pFloatInt2 = pFloatInt |> Point.moveBy 3 4
+    printfn "pFloatInt: %A pFloatInt2: %A" pFloatInt pFloatInt2
--- README.md
+++ README.md
@@ -8,3 +8,4 @@ Learning based on books, courses, fail & wins :)
 Terms: 
 * Higher order functions - function which takes one or more functions as args
 * Type-test operator - if error after :? reproduced -> next action after arrow execute
+* Inline parameter - compiler make copy of func each time they called
